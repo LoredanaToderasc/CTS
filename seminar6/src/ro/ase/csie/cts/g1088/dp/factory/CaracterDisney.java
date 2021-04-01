7b8d347cd98188505708f8ce220e50af97793026
Loredana <toderascloredana18@stud.ase.ro>
2021-04-01 13:28:00 +0300

    Implementare Simple Factory

---
+++ seminar6/src/ro/ase/csie/cts/g1088/dp/factory/CaracterDisney.java
@@ -0,0 +1,26 @@
+package ro.ase.csie.cts.g1088.dp.factory;
+
+public class CaracterDisney extends CaracterAbstract {
+
+	String culoare;
+	boolean okPentruCopiiMici = true;
+
+	@Override
+	public void alearga() {
+		System.out.println("Alearga ...");
+
+	}
+
+	@Override
+	public void sare() {
+		System.out.println("Sare ...");
+
+	}
+
+	public CaracterDisney(String culoare, String nume) {
+		super();
+		this.nume = nume;
+		this.culoare = culoare;
+	}
+
+}
